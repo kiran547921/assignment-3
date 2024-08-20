893d467c5acfec0921fbfe0e40b1ae135a1bb88e
kiran547921 <162077516+kiran547921@users.noreply.github.com>
2024-08-20 21:14:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function srollhome(){
+  document.getElementById('home').scrollIntoView()
+}
+function srollabout(){
+  document.getElementById('about').scrollIntoView()
+}
+function srollproject(){
+  document.getElementById('projects').scrollIntoView()
+}
+function srollcont(){
+  document.getElementById('contact').scrollIntoView()
+}
